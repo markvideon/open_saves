1b67b325f734a316f7f17c5d197d72d9ccea91f6
markvideon <mark.videon@gmail.com>
2022-12-30 20:25:46 +1030

    Clean Dart project

---
+++ example/open_saves_example.dart
@@ -0,0 +1,6 @@
+import 'package:open_saves/open_saves.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/open_saves.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library open_saves;
+
+export 'src/open_saves_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
